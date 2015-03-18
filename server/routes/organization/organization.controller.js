fa12c5e9a7b06a393f27f22ce183111ddd30efe0
Richard Marbach <rmarbach@gmail.com>
2015-03-18 16:24:05 +0200

    Added Organisation endpoint

--- server/routes/organization/organization.controller.js
+++ server/routes/organization/organization.controller.js
@@ -1,13 +1,46 @@
 'use strict';
 
 var Organization = require('./organization.model');
+var NotFoundError = require('../../lib/errors').NotFound;
 
 exports.index = function(req, res, next) {
-  Organization.find({})
+  console.log(req.query);
+  Organization.find(req.query)
     .populate('events')
     .exec(function(err, organizations) {
       if (err) return next(err);
 
       res.json(organizations);
     });
+};
+
+exports.show = function(req, res, next) {
+  var id = req.params.orgId;
+
+  Organization.findById(id, function(err, organization) {
+    if (err) next(err);
+    if (!organization) next(new NotFoundError('No Organization with this id.'));
+
+    res.json(organization);
+  });
+};
+
+exports.update = function(req, res, next) {
+  var id = req.params.orgId;
+
+  Organization.findById(id, function(err, organization) {
+    if (err) return next(err);
+    if (!organization) return next(new NotFoundError('No Organization with that id.'));
+
+    res.json(organization);
+  });
+};
+
+exports.create = function(req, res, next) {
+  var newOrganization = req.body;
+  Organization.create(newOrganization, function(err, organization) {
+    if (err) return next(err);
+
+    res.status(201).json(organization);
+  });
 };
