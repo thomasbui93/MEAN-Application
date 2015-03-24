87b16a363613f74899bfff069e8743a16dbc9192
thomasbui93 <thomasbui93@gmail.com>
2015-03-24 10:31:40 +0200

    home controller again

--- public/app/home/home.controller.js
+++ public/app/home/home.controller.js
@@ -2,5 +2,9 @@
 
 angular.module('voluntr')
   .controller('homeController', function($scope) {
+<<<<<<< HEAD
+    $scope.title = 'Home';
+=======
     $scope.title = "Home";
+>>>>>>> origin/master
   });
