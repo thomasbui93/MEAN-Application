1d219fe98aee061e927711b7a9a8435bcef11b9d
Richard Marbach <rmarbach@gmail.com>
2015-03-25 08:46:11 +0200

    Recruitment organisation from array

--- server/routes/event/index.js
+++ server/routes/event/index.js
@@ -10,4 +10,9 @@ router.post('/', controller.create);
 router.put('/:eventId', controller.update);
 router.delete('/:eventId', controller.remove);
 
+router.get('/:eventId/createdBy');
+router.get('/:eventId/organisation');
+router.get('/:eventId/participants');
+router.get('/:eventId/comments');
+
 module.exports = router;
