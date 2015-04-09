eca38db53a913d421999e1bd8f13dce79b2e91b9
thomasbui93 <thomasbui93@gmail.com>
2015-04-09 16:11:49 +0300

    fix the slow performance in the start with vertical menu

--- public/app/main.controller.js
+++ public/app/main.controller.js
@@ -2,10 +2,10 @@
  * Created by Bui Dang Khoa on 4/9/2015.
  */
 'use strict';
-angular.module('voluntr').controller('mainController', function ($scope) {
-    $scope.menu = false;
-    $scope.toggleMenu = function () {
-        $scope.menu =! $scope.menu;
-        console.log('click');
-    };
+angular.module('voluntr').controller('mainController', function($scope) {
+  $scope.menu = false;
+  $scope.toggleMenu = function() {
+    angular.element(document.querySelector('#menu-vertical')).css({'display': 'block'});
+    $scope.menu = !$scope.menu;
+  };
 });
