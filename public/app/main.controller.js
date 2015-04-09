a93ef262e788d482f3b40d78e68e15c74e27f509
thomasbui93 <thomasbui93@gmail.com>
2015-04-09 16:36:58 +0300

    menu bar

--- public/app/main.controller.js
+++ public/app/main.controller.js
@@ -2,10 +2,11 @@
  * Created by Bui Dang Khoa on 4/9/2015.
  */
 'use strict';
-angular.module('voluntr').controller('mainController', function($scope) {
+angular.module('voluntr').controller('mainController', function($scope, $rootScope) {
   $scope.menu = false;
   $scope.toggleMenu = function() {
     angular.element(document.querySelector('#menu-vertical')).css({'display': 'block'});
     $scope.menu = !$scope.menu;
   };
+  $scope.user = $rootScope.user;
 });
